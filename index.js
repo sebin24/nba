18ac14bdc97598561ae4b23108e4c9b55b7f3e80
sebini24 <sebi.nguyen@googlemail.com>
2018-10-11 18:23:56 +0200

    try 4

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ mongoose     = require('mongoose'),
 TeamInfo     = require('./API/Models/TeamInfo'), //created model loading here
 GameSchedule = require('./API/Models/GameSchedule');
 const PORT = process.env.PORT || 5000
-const path = require('path')
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
 mongoose.connect(config.dbUrl);
@@ -16,9 +15,5 @@ server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 var routes = require('./API/Routes/Routes'); //importing route
 routes(server); //register the route
-server.use(express.static(path.join(__dirname, 'public')))
-server.set('views', path.join(__dirname, 'views'))
-server.set('view engine', 'ejs')
-server.get('/', (req, res) => res.render('pages/index'))
 server.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 ;
